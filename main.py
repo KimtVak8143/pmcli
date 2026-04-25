f2dcd734202c5e982a4def7779ff3337ec2e36a3
Mohit Kumar Kushwaha <cloudsuffers@gmail.com>
2026-04-26 02:07:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from commands.add import add
 from commands.get import get
 from commands.list_cmd import list_sites
 from commands.reveal import reveal
+from commands.start import start
 
 app = typer.Typer(help="PMCLI - Your local password manager")
 
@@ -11,6 +12,7 @@ app.command()(add)
 app.command()(get)
 app.command(name="list")(list_sites)
 app.command()(reveal)
+app.command()(start)
 
 
 if __name__ == "__main__":
